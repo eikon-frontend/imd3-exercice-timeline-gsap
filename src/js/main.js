e94e49f4f64b1b76780ea6aeed76d62244a499f3
iwolfisberg <iris.wolfisberg@edufr.ch>
2024-09-10 18:17:49 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+/* ----------------------------------------------
+    Exercices JavaScript : animer avec
+    la bibliothèque GSAP
+   ----------------------------------------------
+    Installer GSAP
+   ---------------------------------------------- */
+
+/* -----------------------
+    Exercice 1
+   -----------------------
+    Au chargement de la page, faites glisser 
+    les images de la gauche (off-screen) vers la 
+    droite les unes après les autres (effet 
+    d'échellonement - en anglais "stagger")
+    pendant 1.5 secondes.
+    
+    PUIS faites tourner les images (rotation)
+    à 360 degrés pendant 1 seconde.
+    
+    SIMULTANEMENT à l'étape précédente, faites que 
+    les images soient légérement plus petites que 
+    leurs tailles actuelles (échelle à 90%).
+   ----------------------- */
+
+/* -----------------------
+    Exercice 2
+   -----------------------
+    Au survol de n'importe quelle image, 
+    augmentez légérement sa taille (échelle à 100%).
+    
+    Lorsque la souris se déplace en dehors de l'image,
+    celle-ci revient à son état inital (échelle à 90%).
+   ----------------------- */
+
+/* -----------------------
+    Exercice 3
+   -----------------------
+    Au clic de n'importe quelle image, celle-ci
+    s'affiche en plein écran pendant 5 secondes.
+    ENSUITE, elle revient à sa taille initiale.
+    
+    Indice: vous aurez peut-être besoin de manipuler
+    la propriété de position de l'image
+   ----------------------- */
